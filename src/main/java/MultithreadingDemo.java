8c576627d02c81ceca6b8bdb4766049da6b063da
w2436494 <2190638312@qq.com>
2022-12-04 23:35:19 +0800

    项目结构搭建

---
+++ src/main/java/MultithreadingDemo.java
@@ -0,0 +1,11 @@
+/*
+ * @author wcy
+ * @date 2022-12-04 23:17
+ * @description
+ */
+public class MultithreadingDemo {
+
+    public static void main(String[] args) {
+        System.out.println("multithreading ");
+    }
+}
