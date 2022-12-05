d556225d7ad1d2d954f8a6d6857011b1eb244240
w2436494 <2190638312@qq.com>
2022-12-05 23:00:06 +0800

    supplyAsync 案例1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+---
+typora-root-url: src\main\resources\staic\images
+---
+
 # multithreading 笔记
+
+## 1、CompletableFuture入门
+
+CompletableFuture在Java8中提出，Java9中增强
+
+![image-20221205225907610](/img.png)
---
+++ src/main/java/completableFuture/util/SmallTool.java
@@ -0,0 +1,27 @@
+package completableFuture.util;
+
+import java.util.StringJoiner;
+
+/*
+ * @author wcy
+ * @date 2022-12-05 22:34
+ * @description
+ */
+public class SmallTool {
+    public static void printTimeAndThread(String message) {
+        String mess = new StringJoiner("\t")
+                .add(String.valueOf(System.currentTimeMillis()))
+                .add(String.valueOf(Thread.currentThread().getName()))
+                .add(String.valueOf(Thread.currentThread().getName()))
+                .add(message).toString();
+        System.out.println(mess);
+    }
+
+    public static void sleepMillis(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
